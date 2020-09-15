c7c1fee89cffed6b9062132764a15a5d851e863a
Gabrielcv98 <gabrialbert98@gmail.com>
2020-09-15 08:52:01 -0500

    base hecha

---
+++ src/main/java/entity/Usuario.java
@@ -0,0 +1,51 @@
+package entity;
+
+public class Usuario {
+
+    private String nombre;
+    private String apellido;
+    private int telefono;
+    private String anexo;
+    private String departamento;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getAnexo() {
+        return anexo;
+    }
+
+    public void setAnexo(String anexo) {
+        this.anexo = anexo;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+}
